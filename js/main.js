b7efbc1b28f3c81fd2342084c49e96fbafb05059
Lucas Rojas <rojas_lucas92@hotmail.com>
2021-09-04 21:26:48 -0300

    agregue main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let valorRemera = 1300;
+let envio = 480;
+let zonaNorte = 50;
+let zonaSur = 300;
+let zonaOeste = 100;
+let capital = 200;
+
+const tuProducto = (valorRemera, envio) =>{
+    if ()
+}
