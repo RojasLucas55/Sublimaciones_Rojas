df1fbf0efc0b9d8d9ab4f33b638355a5066e359c
Lucas Rojas <rojas_lucas92@hotmail.com>
2021-09-07 21:17:46 -0300

    simulador de compra/envio

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-let valorRemera = 1300;
-let envio = 480;
-let zonaNorte = 50;
-let zonaSur = 300;
-let zonaOeste = 100;
-let capital = 200;
+const suma  = (a,b) => a + b;
+const resta = (a,b) => a - b;
 
-const tuProducto = (valorRemera, envio) =>{
-    if ()
+// Variables con precios de producto y envio
+let precioRemera  = 1300; 
+let precioEnvios = 580;
+let valorTotal = suma(precioRemera, precioEnvios);
+// Solo si el comprador es de GBA se le hace un descuento de $100
+let descuento = prompt( "Es Usted de GBA?");
+if((descuento.toLocaleLowerCase() == "si")){
+    console.log("El total de su compra es de: ", resta(valorTotal, 100));
+}else{
+    console.log("El total de su compra es de: ", valorTotal)
 }
+
+
