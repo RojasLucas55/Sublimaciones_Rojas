3a15d2919e6d1964c86bf5f6025ed45f5891f95c
Lucas Rojas <rojas_lucas92@hotmail.com>
2021-09-10 21:11:55 -0300

    Agregue Objetos y Arrays al desafio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-const suma  = (a,b) => a + b;
+/*const suma  = (a,b) => a + b;
 const resta = (a,b) => a - b;
+
 
 // Variables con precios de producto y envio
 let precioRemera  = 1300; 
@@ -12,5 +13,65 @@ if((descuento.toLocaleLowerCase() == "si")){
 }else{
     console.log("El total de su compra es de: ", valorTotal)
 }
+*/
 
+alert ("Bienvenido/a a Luxana's Road")
+let nombreCliente = prompt (" Por favor, ingresa tu nombre para una mejor atención")
+
+const catalogo = [
+
+    producto1 = {
+        id: 1,
+        nombre: "Remeras Personalizadas",
+        precio: 1300,
+    },
+
+    producto2 = {
+        id: 2,
+        nombre: "Tazas",
+        precio: 550,
+    },
+
+    producto3 = {
+        id: 3,
+        nombre: "Fundas de Celulares",
+        precio: 650,
+    },
+
+    producto4 = {
+        id: 4,
+        nombre: "Cuadros Personalizados",
+        precio: 1000,
+    } 
+];
+
+let total = 0;
+let carrito = [] ;
+console.log(carrito);
+
+function aniadirAlCarrito(producto){
+    carrito.push(producto);
+}
+
+aniadirAlCarrito(producto2);
+aniadirAlCarrito(producto3);
+aniadirAlCarrito(producto1);
+aniadirAlCarrito(producto4);
+
+console.log(carrito);
+
+function calcularTotal(){
+
+    for (let i = 0; i < carrito.length; i++) {
+        total = total+carrito[i].precio
+    }
+    
+    console.log(total);
+}
+
+alert ("Usted tiene elementos en su carrito, desea conocer el total de su compra? ")
+
+calcularTotal()
+
+console.log(carrito);
 
